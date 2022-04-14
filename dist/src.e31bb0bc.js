21459753a1c1eefee42fe7df778416a111677d60
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2022-04-13 17:51:30 -0700

    Added an image

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8207,7 +8207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64719" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50332" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
